41765674c03d4f2eff6c88f1bb24829e2d44f06e
drktao <83025663+drktao@users.noreply.github.com>
2023-09-20 21:10:04 +0000

    fix error

--- scripts/lib.py
+++ scripts/lib.py
@@ -27,7 +27,7 @@ def visualize_hist(data, feature):
     plt.xlabel("Score")
     plt.ylabel("Frequency")
     plt.show()
-    plt.savefig("denirohist.png")
+    plt.savefig("hist_viz.png")
     return
 
 
